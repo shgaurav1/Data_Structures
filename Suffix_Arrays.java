32dd7a142d030e2c0306652866ebbcacccf9e7d1
Gaurav Shrivastava <gaurav.gaurav.shrivastava@gmail.com>
2015-09-16 21:40:22 +0530

    Update Suffix_Arrays.java

--- Suffix_Arrays.java
+++ Suffix_Arrays.java
@@ -2,6 +2,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+
+class Suffix{
+
+	public String element;
+	int index;
+}
 class Suffix_Arrays {
 
 	/**
@@ -11,11 +17,12 @@ class Suffix_Arrays {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		String buffer = in.readLine();
 		int n = buffer.length();
-		String SuffixArray[] = new String[n];
+		Suffix suffixArray[] = new Suffix[n];
 		for (int i = 0; i < n; i++) {
-			SuffixArray[i] = buffer.substring(i, n);
+			suffixArray[i].element = buffer.substring(i, n);
+			suffixArray[i].index = i;
 		}
-		Arrays.sort(SuffixArray);
+		Arrays.sort(suffixArray);
 
 	}
 
